c5db8ae43645acde1f31f43b5b6353720131440a
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2026-06-08 10:31:39 -0300

    Add barcode function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+## 0.0.5
+* Create Barcode Supporting by [Pedro Souza](https://github.com/sousa-p)
 ## 0.0.4
 * Improve sensitive_content to have a better approach
 ## 0.0.3
--- lib/customs/custom_barcode.dart
+++ lib/customs/custom_barcode.dart
@@ -53,12 +53,17 @@ class CustomBarcode extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height =
-        (int.tryParse(barcode['size'].toString()) ?? 80).toDouble();
+     final double height = (barcode['size'] is num
+             ? (barcode['size'] as num).toDouble()
+             : double.tryParse(barcode['size']?.toString() ?? '')) ??
+         80.0;
+
+          final String data = (barcode['content'] ?? '').toString();
+     if (data.isEmpty) return const SizedBox.shrink();
     return Center(
       child: BarcodeWidget(
         barcode: _resolveType(barcode['type']),
-        data: barcode['content'] ?? '',
+        data: data,
         height: height,
         drawText: _resolveDrawText(barcode['drawText']),
         errorBuilder: (context, error) => const SizedBox.shrink(),
